adafebe8830197aadf5b4cfa9113ecc882970904
caemci <carlosecimino@gmail.com>
2020-10-23 23:10:53 -0300

    Quitado boton 'Agregar'

--- src/GifSearcherApp.js
+++ src/GifSearcherApp.js
@@ -5,17 +5,11 @@ export const GifSearcherApp = () => {
 
 	const [categories, setCategories] = useState(['Musica', 'Futbol', 'Peliculas']);
 
-	const handleAdd = () => {
-		setCategories([...categories, 'Viajes'])
-	}
-
 	return (
 		<>
 			<h2>GifSearcherApp</h2>
 			<AddCategory setCategories={setCategories} />
 			<hr />
-
-			<button onClick={handleAdd}>Agregar</button>
 
 			<ol>
 				{
