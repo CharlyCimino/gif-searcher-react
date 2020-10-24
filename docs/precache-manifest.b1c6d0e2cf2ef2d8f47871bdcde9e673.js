fc664de160c15f638837021ab641f1e8e84c7dd0
caemci <carlosecimino@gmail.com>
2020-10-24 00:55:03 -0300

    Build de produccion

---
+++ docs/precache-manifest.b1c6d0e2cf2ef2d8f47871bdcde9e673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8a35190bdd473a1a1e8e5422a51de8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56c8a1dcd2fe1f614d19",
+    "url": "/static/css/main.29c3df99.chunk.css"
+  },
+  {
+    "revision": "65e0434ce11e780c63b4",
+    "url": "/static/js/2.592b18ff.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.592b18ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56c8a1dcd2fe1f614d19",
+    "url": "/static/js/main.1f823c20.chunk.js"
+  },
+  {
+    "revision": "f71bbb20ac58de153d45",
+    "url": "/static/js/runtime-main.f7aa422f.js"
+  }
+]);
